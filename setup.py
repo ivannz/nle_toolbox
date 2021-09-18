1d46a1ab5921426fa6e6b6760e77d94668a01392
ivannz <ivannz@yandex.ru>
2021-09-18 15:37:21 +0300

    package skeleton commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    # update the version number
+    version = open('version', 'r').read().strip()
+
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(cwd, 'nle_tools', '__version__.py'), 'w') as f:
+        f.write(f'__version__ = \'{version}\'\n')
+
+    setup(
+        name='nle_tools',
+        version=version,
+        description="""Toolbox for NLE Challenge""",
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        license='MIT',
+        packages=[
+            'nle_tools',
+        ],
+    )

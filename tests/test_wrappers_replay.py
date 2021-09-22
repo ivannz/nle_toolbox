f744e8b39c4786b067b0825a4a4a59ac962225fd
Ivan Nazarov <ivannz@yandex.ru>
2021-09-23 00:50:22 +0300

    update the unit tests for .replay and load-state-dict

--- tests/test_wrappers_replay.py
+++ tests/test_wrappers_replay.py
@@ -9,6 +9,31 @@ from random import choices
 from nle_toolbox.wrappers.replay import Replay
 
 
+def replay(env, actions, *, seed):
+    history, obs_, gen = [], None, env.replay(actions, seed=seed)
+    try:
+        while True:
+            obs, act, rew, obs_, info = next(gen)
+            history.append(obs)
+
+    except StopIteration as e:
+        remaining = e.value
+
+    # write the last observation, unless nothing has been written at all.
+    if history:
+        history.append(obs_)
+
+    return history, remaining
+
+
+def collate(records):
+    return {
+        k: numpy.array([
+            rec[k] for rec in records
+        ]) for k in records[0]
+    }
+
+
 @pytest.mark.parametrize(
     "k", [1, 10, 50, 100, 10000],
 )
@@ -17,19 +42,26 @@ def test_utils_seeding_set_seed(k):
     actions = choices(range(16), k=k)
 
     with Replay(gym.make("NetHackChallenge-v0")) as env:
-        history, remaining1 = env.replay(actions, seed=None)
+        history, remaining1 = replay(env, actions, seed=None)
+        obs_trace1 = collate(history)
+
         state_dict = env.state_dict()
 
-        obs_trace1 = {
-            k: numpy.array([o[k] for o in history]) for k in history[0]
-        }
+    ref = history[-1]
+    with Replay(gym.make("NetHackChallenge-v0")) as env:
+        obs = env.load_state_dict(state_dict)
+
+    assert all(
+        (obs[k] == ref[k]).all() for k in obs.keys() | ref.keys()
+    )
 
     with Replay(gym.make("NetHackChallenge-v0")) as env:
-        history, remaining2 = env.load_state_dict(state_dict)
-
-        obs_trace2 = {
-            k: numpy.array([o[k] for o in history]) for k in history[0]
-        }
+        history, remaining2 = replay(
+            env,
+            state_dict['actions'],
+            seed=state_dict['seed'],
+        )
+        obs_trace2 = collate(history)
 
     # check the traces
     assert remaining1 == remaining2

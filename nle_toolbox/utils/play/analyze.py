0ba93b520acf7752facb6e88c0eb7230b3e7668c
Ivan Nazarov <ivannz@yandex.ru>
2021-10-01 13:34:01 +0300

    factor series defns out from the eval proc, allow secondary series for a scatter plot, make it so that cache is recalced if a series is missing

--- nle_toolbox/utils/play/analyze.py
+++ nle_toolbox/utils/play/analyze.py
@@ -15,6 +15,77 @@ from ...wrappers.replay import Replay
 from ..obs import BLStats
 
 
+"""The series we compute for each trace."""
+SERIES = {
+    'reward__total':
+        lambda rew, bls: float(
+            rew.sum()
+        ),
+
+    'reward__sparsity':
+        lambda rew, bls: float(
+            (rew == 0).mean()
+        ),
+
+    'blstats__hitpoints__mean':
+        lambda rew, bls: float(
+            bls.hitpoints.mean()
+        ),
+
+    'blstats__hitpoints__median':
+        lambda rew, bls: float(
+            numpy.median(bls.hitpoints)
+        ),
+
+    'blstats__hitpoints__std':
+        lambda rew, bls: float(
+            bls.hitpoints.std()
+        ),
+
+    'blstats__experience_points__max':
+        lambda rew, bls: float(
+            bls.experience_points.max()
+        ),
+
+    'blstats__level_number__max':
+        lambda rew, bls: float(
+            bls.level_number.max()
+        ),
+
+    'blstats__energy__mean':
+        lambda rew, bls: float(
+            bls.energy.mean()
+        ),
+
+    'blstats__energy__std':
+        lambda rew, bls: float(
+            bls.energy.std()
+        ),
+
+    'blstats__gold__total':
+        lambda rew, bls: float(
+            bls.gold.sum()
+        ),
+
+    'blstats__score__max':
+        lambda rew, bls: float(
+            bls.score.max()
+        ),
+
+    'blstats__score__per_time':
+        lambda rew, bls: float(
+            bls.score.max() / bls.time.max()
+        ),
+
+    # frequency of various hunger states
+    'blstats__hunger_state__counts':
+        lambda rew, bls: numpy.bincount(
+            bls.hunger_state,
+            minlength=7,
+        ).tolist(),
+    }
+
+
 def traces(path, ext='.pkl'):
     """Get all replays in the specified folder."""
 
@@ -66,7 +137,10 @@ def simulate_one(state_dict):
     blstats = BLStats(*numpy.array(blstats).T)
 
     # return the info at termination, and the collated stats
-    return info, (numpy.array(rew), blstats,)
+    return info, (
+        numpy.array(rew),
+        blstats,
+    )
 
 
 def evaluate_one(sh, trace, state_dict):
@@ -76,37 +150,21 @@ def evaluate_one(sh, trace, state_dict):
         info, (rewards, blstats, *ignored) = simulate_one(state_dict)
 
     except ValueError:
-        return sh, (trace, {})
+        return sh, dict(trace=trace, data={})
 
-    # compute the statistics
-    numeric = {
-        'reward__total': rewards.sum(),
-        'reward__sparsity': (rewards == 0).mean(),
-        'blstats__hitpoints__mean': blstats.hitpoints.mean(),
-        'blstats__hitpoints__std': blstats.hitpoints.std(),
-        'blstats__energy__mean': blstats.energy.mean(),
-        'blstats__energy__std': blstats.energy.std(),
-        'blstats__gold__total': blstats.gold.sum(),
-        'blstats__score__max': blstats.score.max(),
-
-        'blstats__score__per_time': blstats.score.max() / blstats.time.max(),
-    }
-
-    # frequency of various hunder states
-    hunger_state = numpy.bincount(blstats.hunger_state, minlength=7).tolist()
-    return sh, (
-        trace, {
-            'blstats__hunger_state__counts': hunger_state,
-            **{k: float(v) for k, v in numeric.items()},
-            **{'info__' + k: v for k, v in info.items()},
+    # compute the statistics and supplement with the final infodict
+    stats = {k: fn(rewards, blstats) for k, fn in SERIES.items()}
+    return sh, dict(
+        trace=trace,
+        data={
+            **stats, **{'info__' + k: v for k, v in info.items()},
         },
     )
 
 
-def evaluate(folder, *, n_jobs=-1, verbose=10):
+def evaluate(folder, n_jobs=-1, verbose=10):
     """Scan the specified folder for replay traces and compute missing stats.
     """
-
     folder = os.path.abspath(folder)
 
     # open the jayson metadata
@@ -118,6 +176,12 @@ def evaluate(folder, *, n_jobs=-1, verbose=10):
     for trace, sh, state_dict in map(read_one, traces(folder)):
         # recompute the cache and the stats
         if sh not in cache:
+            unready[sh] = trace, state_dict
+            continue
+
+        # make sure the data contains at least the series specified at the top
+        data = cache[sh]['data']
+        if data and (SERIES.keys() - data.keys()):
             unready[sh] = trace, state_dict
 
     if unready:
@@ -131,19 +195,19 @@ def evaluate(folder, *, n_jobs=-1, verbose=10):
     return cache
 
 
-def series(cache, name='blstats__score__max'):
+def series(cache, *names):
     """Extract the specified data from the cache."""
+    names = list(filter(None, names))
 
     data = []
-    for trace, stats in cache.values():
-        if name not in stats:
+    for rec in cache.values():
+        trace, stats = rec['trace'], rec['data']
+        if not all(n in stats for n in names):
             continue
 
-        data.append((
-            trace, stats[name]
-        ))
+        data.append((trace, *(stats[n] for n in names)))
 
-    traces, data = zip(*data)
+    traces, *data = zip(*data)
     return traces, numpy.array(data)
 
 
@@ -151,6 +215,7 @@ def main(
     folder,
     name='blstats__score__max',
     q=0.1,
+    aux=None,
     *,
     plot=False,
     debug=False,
@@ -161,19 +226,29 @@ def main(
     cache = evaluate(folder)
 
     try:
-        traces, scores = series(cache, name)
+        traces, (main, *rest) = series(cache, name, aux)
 
     except ValueError:
         print(f"\n\nNo data for `{name}` in `{folder}`. Aborting...\n\n")
         return []
 
     # get the threshold and display a histogram
-    lo = numpy.quantile(scores, q=q)
+    lo = numpy.quantile(main, q=q)
     if plot:
         fig, ax = plt.subplots(1, 1, figsize=(4, 3), dpi=160)
-        ax.set_title(f"Histogram for `{name}`")
-        ax.hist(scores, bins=51, alpha=0.75)
+        if aux:
+            yaxis, *ignored = rest
+            ax.set_title('Scatter plot')
+            ax.scatter(main, yaxis, alpha=0.75)
+            ax.set_ylabel(aux)
+
+        else:
+            ax.set_title(f"Histogram for `{name}`")
+            ax.hist(main, bins=51, alpha=0.75)
+
+        ax.set_xlabel(name)
         ax.axvline(lo, c='k', zorder=10)
+        fig.tight_layout()
         fig.show()
 
         print("\n\nClick anywhere on the plot to continue...\n\n")
@@ -181,7 +256,7 @@ def main(
         plt.close()
 
     return [
-        tr for tr, sc in zip(traces, scores) if sc <= lo
+        (sc, tr) for tr, sc in zip(traces, main) if sc <= lo
     ]
 
 
@@ -202,6 +277,10 @@ if __name__ == '__main__':
         help='The score series to analyze.')
 
     parser.add_argument(
+        '--aux', type=str, default=None, required=False,
+        help='The auxiliary series on the y-axis of a scatter plot.')
+
+    parser.add_argument(
         '--q', type=float, default=0.1, required=False,
         help='The quantile [0, 1] for the most underperforming.')
 
@@ -209,7 +288,12 @@ if __name__ == '__main__':
         '--debug', required=False, dest='debug', action='store_true',
         help='Enter trace mode.')
 
-    parser.set_defaults(q=0.1, name='blstats__score__max', debug=False)
+    parser.set_defaults(
+        q=0.1,
+        name='blstats__score__max',
+        aux=None,
+        debug=False,
+    )
 
     args, _ = parser.parse_known_args()
     pp.pprint(main(**vars(args), plot=True))

9ebcfa569dbee5fc19191a0dbd5e7ea8bcf61671
ivannz <ivannz@yandex.ru>
2021-10-14 12:16:48 +0300

    fixup poor tty rendering of multiline messages by the NLE

--- nle_toolbox/utils/env/render.py
+++ nle_toolbox/utils/env/render.py
@@ -1,4 +1,84 @@
-def render(tty_colors, tty_chars, tty_cursor, **ignore):
+import numpy as np
+
+from textwrap import wrap
+
+from ..obs import Misc
+
+
+def fixup_tty(
+    *,
+    tty_chars,
+    tty_colors,
+    tty_cursor,
+    message,
+    chars,
+    colors,
+    misc,
+    **ignore,
+):
+    r"""Restore broken `tty_chars` in case of mutliline message with `--More--`.
+
+    Details
+    -------
+    It apperas that the tty used internally byt the NLE to capture NetHack's
+    output operates in CRLF (\r\n) mode, whereas the game itself issues LF (\n).
+    This results in broken second and third lines when the message in long
+    and was wrapped at 80 char per column by the game. the NLE gives us plenty
+    of information to attempt to reconstruct the correct tty-chars output
+    """
+
+    # message containing an LF means that originally it did not fit 80 cols
+    lf_mask = message == 0x0A
+    misc = Misc(*misc.astype(bool))
+    if lf_mask.any() and misc.xwaitingforspace:
+        # fix the message: replace lf '\n' (\x0a) with whitespace ` ` (\x20)
+        message = np.where(lf_mask, 0x20, message)
+
+        # properly wrap the text at 80
+        text = message.view('S256')[0].decode('ascii')
+        pieces = wrap(text + '--More--', 80)
+        if len(pieces) != 2:
+            raise RuntimeError(f"Message `{text}` is too long.")
+
+        # recreate tty-chars by patching the relevant regions
+        new_tty_chars, new_tty_colors = np.copy(tty_chars), np.copy(tty_colors)
+
+        # fix the game's 21x79 viewport [1:22, :79], multiline messages may
+        #  encroach on the top line of the viewport.
+        new_tty_chars[1:22, :79] = chars
+        new_tty_colors[1:22, :79] = colors
+
+        # patch the top two lines. We may assume the colors are correct, since
+        #  the original tty_chars had at least three top lines affected.
+        vw_new_tty_chars = new_tty_chars.view('S80')
+        for r, line in enumerate(pieces):
+            vw_new_tty_chars[r] = bytes(f'{line:<80s}', encoding='ascii')
+
+        # leave the bottom line stats line intact
+        pass
+
+        # patch the cursor to the last non-whitespace char in line 1
+        new_tty_cursor = np.copy(tty_cursor)
+        new_tty_cursor[:] = 1, len(pieces[1])
+
+        # replace with the patched data
+        tty_chars, tty_colors = new_tty_chars, new_tty_colors
+        tty_cursor = new_tty_cursor
+
+    return dict(
+        tty_chars=tty_chars,
+        tty_colors=tty_colors,
+        tty_cursor=tty_cursor,
+    )
+
+
+def render(
+    *,
+    tty_chars,
+    tty_colors,
+    tty_cursor,
+    **ignore,
+):
     """Render the observation form the NLE using ANSI escapes.
 
     Details

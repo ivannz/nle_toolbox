120db38ba200fa44c3b0e039bf0d76254af69b4f
ivannz <ivannz@yandex.ru>
2021-10-14 17:26:40 +0300

    WIP let handler throe exceptions whenever it cannot handle an observation

--- nle_toolbox/bot/overrider.py
+++ nle_toolbox/bot/overrider.py
@@ -3,18 +3,22 @@ from inspect import isgenerator
 from gym import Wrapper
 
 
-def interact(env, *, can, handle):
+class UnhandledObservation(Exception):
+    """Signal the caller that an Overrider cannot deal with the data."""
+    pass
+
+
+def interact(env, *, handle):
     """A simple semi-automatic interaction loop. See `doc/shell`."""
     # construct the initial result
     obs, rew, done, info = env.reset(), 0., False, {}
     while not done:
         # if we cannot override then yield to the user
-        if not can(obs, info=info):
-            act = yield obs, rew, done, info
-
-        else:
-            # handle stuff
+        try:
             act = handle(obs, rew, done, info)
+
+        except UnhandledObservation:
+            act = yield obs, rew, done, info
 
         # interact with the environment
         obs, rew, done, info = env.step(act)
@@ -31,20 +35,14 @@ class BaseOverrider(Wrapper):
             self._loop.close()
 
         # reset the generator and start it
-        self._loop = interact(
-            self.env,
-            can=self.can,
-            handle=self.handle,
-        )
-
+        self._loop = interact(self.env, handle=self.handle)
         obs, rew, done, info = self._loop.send(None)
         return obs
 
     def step(self, act):
         return self._loop.send(act)
 
-    def can(self, obs, rew=0., done=False, info=None):
-        return False
-
     def handle(self, obs, rew=0., done=False, info=None):
-        raise NotImplementedError
+        """Throwing an exception allows partial handling: we can update
+        internal state and data, yet still yield to the user."""
+        raise UnhandledObservation

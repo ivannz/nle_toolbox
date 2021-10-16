6a7528b22ab27816265ab23ce5ad3a7dbd29abf2
ivannz <ivannz@yandex.ru>
2021-10-17 00:51:48 +0300

    add peer and follower modes in BaseOverrider

--- nle_toolbox/bot/overrider.py
+++ nle_toolbox/bot/overrider.py
@@ -1,4 +1,5 @@
 from inspect import isgenerator
+from nle_toolbox.bot.genfun import is_suspended
 
 from gym import Wrapper
 
@@ -8,15 +9,16 @@ class UnhandledObservation(Exception):
     pass
 
 
-def interact(env, *, handle):
-    """A simple semi-automatic interaction loop. See `doc/shell`."""
+def interact(env, *, peer):
+    """A simple semi-automatic interaction loop in peer mode. See `doc/shell`.
+    """
     # construct the initial result
     obs, rew, done, info = env.reset(), 0., False, {}
     while not done:
         # if we cannot override then yield to the user
         # XXX `done` is always False inside the loop, so we can hardcode it
         try:
-            act = handle(obs, rew, False, info)
+            act = peer(obs, rew, False, info)
 
         except UnhandledObservation:
             act = yield obs, rew, False, info
@@ -28,16 +30,58 @@ def interact(env, *, handle):
     return obs, rew, True, info
 
 
+def operate(env, *, follower):
+    """A simple semi-automatic interaction loop in follower mode.
+    See `doc/shell`.
+    """
+    # construct the initial result
+    tx = obs, rew, done, info = env.reset(), 0., False, {}
+
+    # `None` command indicates startup
+    gen, n_yields, n_steps = follower(*tx, cmd=None), 0, 0
+    while not done:
+        try:
+            act = gen.send(tx if is_suspended(gen) else None)
+
+        except StopIteration:
+            # We have initialized the generator with the same `tx` data.
+            # Therefore we halt, if we get two immediate `StopIteration`-s
+            # in a row, i.e. without any intermediate `.step`-s.
+            if n_yields > 0 and n_steps < 1:
+                # XXX maybe raise a warning instead?
+                raise RuntimeError
+
+            gen = follower(*tx, cmd=(yield obs, rew, False, info))
+            n_yields += 1
+            continue
+
+        tx = obs, rew, done, info = env.step(act)
+        n_steps += 1
+
+    # `done=True` is important enough to bubble upstream
+    return obs, rew, True, info
+
+
 class BaseOverrider(Wrapper):
     """Gym interface for overrider."""
     _loop = None
+
+    def __init__(self, env, *, mode='peer'):
+        assert mode in ('peer', 'follow')
+        super().__init__(env)
+
+        self.is_follow = mode == 'follow'
 
     def reset(self, **kwargs):
         if isgenerator(self._loop):
             self._loop.close()
 
         # reset the generator and start it
-        self._loop = interact(self.env, handle=self.handle)
+        if self.is_follow:
+            self._loop = operate(self.env, follower=self.handle)
+        else:
+            self._loop = interact(self.env, peer=self.handle)
+
         try:
             obs, rew, done, info = self._loop.send(None)
             return obs
@@ -52,7 +96,7 @@ class BaseOverrider(Wrapper):
         except StopIteration as e:
             return e.value
 
-    def handle(self, obs, rew=0., done=False, info=None):
+    def handle(self, obs, rew=0., done=False, info=None, cmd=None):
         """Throwing an exception allows partial handling: we can update
         internal state and data, yet still yield to the user."""
         raise UnhandledObservation

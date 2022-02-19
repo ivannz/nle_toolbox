95e036c2ade0d1f56a0535aebc9c448792ca280b
Ivan Nazarov <ivannz@yandex.ru>
2022-02-19 03:15:23 +0300

    WIP redid the api to capsule; made the calls to step and do (learn) have more uniofrm api; updated the docs

--- nle_toolbox/utils/rl/capsule.py
+++ nle_toolbox/utils/rl/capsule.py
@@ -20,28 +20,39 @@ def launch(capsule, initial):
     return capsule.send(initial)
 
 
-def capsule(learner, n_fragment_length=20, f_h0_lerp=0.):
+def capsule(step, do, length, *, h0=None, alpha=0.):
     """T-BPTT trajectory collector for capsuled RL. See docs `.engine.step`.
 
-    learner must be a callable object accepting kwargs
-        `obs, act, rew, fin, hx`,
-    returning `act, output, upd_hx` (diff-able), auto-initialising `hx` if
-    necessary. Optionally the object could have the `.initial_hx` property
-    for the `hx` in the very first call. Here `hx` is the recurrent (runtime)
-    state of the learner.
+    Parameters
+    ----------
+    step: callable
+        A funciton taking `input` (namedtuple with fields `obs`, `act`, `rew`,
+        and `fin`, which represents the recent observation) and the keyword
+        `hx` (arbitrarily nested tensors, which store the recurrent runtime
+        state, which may be auto-initialised if `hx` is None. `step` returns
+        the action `act`, auxiliary data `output`, and the new state `hx`.
 
-    It must also have a method `.learn`, which takes in `obs , act, rew, fin`
-    input (always non-diffable), `output` (diff-able depending on the method)
-    and `hx` (could be anything, depending on the arch of the learner) and
-    updates the internal parameters (possibly including the value in
-    `.initial_hx`).
+    do: callable
+        A function that updates whatever internal parameters `step` depends on,
+        and takes in `input` (always non-diffable `obs`, `act`, `rew`, `fin`),
+        `output` and `hx` (both possibly diff-able).
+
+    length: int
+        The length of trajectory fragments used for each truncated bptt grad
+        update.
+
+    h0: nested tensors
+        The initial learnable runtime state `h0`.
+
+    alpha: float
+        The between-fragment blending coefficient, which allows some feedback
+        (learning) to `h0` from `hx` used in the collected fragment in T-BPTT.
     """
-    # (capsule) get the initial recurrent (runtime) state and `.learn` of the learner
-    do_learn = learner.learn
-    h0 = getattr(learner, 'initial_hx', None)
+    if do is None and length >= 1 or do is not None and length < 1:
+        raise ValueError('`do` can be None iff fragment `length` is zero.')
 
     # (sys) let the learner properly init `hx`-s batch dims, since `h0` may
-    #  have unitary batch dim
+    #  have unitary batch dims
     hx_pre = hx = None
 
     # (capsule) finish handshake and prepare the npyt state
@@ -52,10 +63,7 @@ def capsule(learner, n_fragment_length=20, f_h0_lerp=0.):
 
     # (sys) collect trajectory in fragments, when instructed to
     fragment = []
-    append = (lambda x: None) if n_fragment_length < 1 else fragment.append
-
-    # (sys) fixup length to make the fragment size check work properly
-    n_fragment_length = max(1, n_fragment_length)
+    append = id if length < 1 else fragment.append  # `id` serves as a dummy
 
     # (sys) perpetual rollout
     while True:  # .learn()
@@ -64,7 +72,7 @@ def capsule(learner, n_fragment_length=20, f_h0_lerp=0.):
 
         # REACT x_t, a_{t-1}, h_t -->> a_t, y_t, h_{t+1}
         #       with `a_t \sim \pi_t`
-        act_, out, hx = learner(**input._asdict(), hx=hx)
+        act_, output, hx = step(input, hx=hx)
 
         # (sys) update the action in `npy` through `pyt`
         suply(torch.Tensor.copy_, pyt.act, act_)
@@ -79,26 +87,25 @@ def capsule(learner, n_fragment_length=20, f_h0_lerp=0.):
         suply(np.copyto, npy.rew, rew_)  # XXX allow structured rewards
         np.copyto(npy.fin, fin_)  # XXX must be a boolean scalar/vector
 
-        append((input, out))
-
         # (sys) collect a fragment of time `t` afterstates t=0..N-1
-        if len(fragment) < n_fragment_length:
+        append((input, output))
+        if len(fragment) < length:
             continue
 
         # (sys) one-step lookahead $y_N$, e.g. value-to-go bootstrap.
         # DO NOT yield action to the caller, nor update `npy-pyt`, nor `hx`!
         input = suply(torch.clone, pyt)
-        _, out, _ = learner(**input._asdict(), hx=hx)
-        fragment.append((input, out))
+        _, output, _ = step(input, hx=hx)
+        append((input, output))
 
         # (sys) repack data ((x_t, a_{t-1}, r_t, d_t), y_t), t=0..N
         # XXX note, `.act[t]` is $a_{t-1}$, but the other `*[t]` are $*_t$,
-        #  e.g. `.rew[t]` is $r_t$, and `out[t]` is `$y_t$
-        input, out = plyr.apply(torch.cat, *fragment, _star=False)
+        #  e.g. `.rew[t]` is $r_t$, and `output[t]` is `$y_t$
+        input, output = plyr.apply(torch.cat, *fragment, _star=False)
         fragment.clear()
 
         # (sys) learn on the collected fragment with `.learn`
-        do_learn(input, out, hx=hx_pre)
+        do(input, output, hx=hx_pre)
 
         if hx is not None:
             # (sys) retain running state `hx`, but DO NOT backprop through it
@@ -107,8 +114,8 @@ def capsule(learner, n_fragment_length=20, f_h0_lerp=0.):
 
             # (sys) pass some grad feedback from the next fragment into `h0`
             #    `.lerp: hx <<-- (1 - w) * hx + w * h0` (broadcasts correctly).
-            if h0 is not None and f_h0_lerp > 0:
-                hx = plyr.apply(torch.lerp, hx, h0, weight=f_h0_lerp)
+            if h0 is not None and alpha > 0:
+                hx = plyr.apply(torch.lerp, hx, h0, weight=alpha)
 
         # (sys) save the recurrent state `hx` at the start of the next fragment
         hx_pre = hx

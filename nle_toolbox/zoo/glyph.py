794dee446093cd3b33b94230bc811c6f9e2dccb6
Ivan Nazarov <ivannz@yandex.ru>
2022-02-24 14:57:04 +0300

    put back the legacy glyph extractor, since it is not tethered to a env wrapper

--- nle_toolbox/zoo/glyph.py
+++ nle_toolbox/zoo/glyph.py
@@ -1,8 +1,19 @@
+from nle.nethack import (
+    MAX_GLYPH,
+    # NO_GLYPH,
+    # NLE_BLSTATS_SIZE,
+    # NLE_INVENTORY_SIZE,
+    NLE_BL_X,
+    NLE_BL_Y,
+)
+
 import torch
 from torch import nn
 
+from einops import rearrange
 from torch.nn import functional as F
 
+from ..utils.nn import bselect
 from ..utils.env.defs import glyphlut, glyph_group, MAX_ENTITY
 
 
@@ -58,3 +69,65 @@ class EgoCentricEmbedding(nn.Module):
         # XXX  `.pad` pads dims from tail
         padding = (0, 0, Hq, Hq + Hr - 1, Wq, Wq + Wr - 1)
         return vicinity.add(F.pad(self.ego, padding))
+
+
+class GlyphFeatureExtractor(nn.Module):
+    """This service layer performs glyph embedding form the inventory and
+    the screen glyphs. From the latter it extracts a vicinity of the specified
+    radius about the coordinates given in the bottom line stats.
+    """
+    def __init__(self, glyphs, window=1):
+        super().__init__()
+        self.glyphs = glyphs
+        self.window = window
+
+    def forward(self, obs):
+        k = self.window
+
+        # turn T x B x R x C into `T x B x (k+R+k) x (k+C+k) x ...`
+        #  by pading symmetrically with invalid glyphs and embedding.
+        glyphs = obs['glyphs']
+        gl_padded = self.glyphs(F.pad(glyphs, (k,) * 4, value=MAX_GLYPH))
+        # XXX we embed extra 2(R + C) + 4 glyphs, i.e. about 13% overhead
+
+        # use stride trix to dimshuffle and unfold into sliding local windows
+        #  of the form `T x B x R x C x ... x (k+1+k) x (k+1+k)`
+        # XXX could we have used einops here? No, as it does not provide
+        #  rearranging dims with overlappng strides.
+        n_seq, n_batch, n_rows, n_cols = glyphs.shape
+        n_features = gl_padded.shape[4:]
+        s_seq, s_batch, s_rows, s_cols, *s_features = gl_padded.stride()
+        gl_windows = gl_padded.as_strided(
+            # fold R x C into 2d windows in dims after F
+            (n_seq, n_batch, n_rows, n_cols, *n_features, k+1+k, k+1+k),
+
+            # when mul-ing dims in shape use the lowest stride!
+            (s_seq, s_batch, s_rows, s_cols, *s_features, s_rows, s_cols),
+        )
+
+        # extract vicinities around the row-col coordinates, specified in bls
+        bls = obs['blstats']
+        gl_vicinity = bselect(
+            gl_windows,
+            bls[..., NLE_BL_Y],
+            bls[..., NLE_BL_X],
+            dim=2,
+        )
+
+        # now permute the embedded glyphs to T x B x ... x R x C
+        gl_screen = rearrange(gl_padded[:, :, k:-k, k:-k],
+                              'T B R C ... -> T B ... R C')
+
+        # embed inventory glyphs
+        # XXX need to replace NO_GLYPH with MAX_GLYPH, unless they coincide.
+        gl_inventory = rearrange(self.glyphs(obs['inv_glyphs']),
+                                 'T B N ... -> T B ... N')
+
+        # final touch is to extract the embedding of `self` on the map
+        gl_self = gl_vicinity[..., k, k]
+        return dict(
+            screen=gl_screen.contiguous(),
+            vicinity=gl_vicinity,  # already contiguous
+            inventory=gl_inventory.contiguous(),
+            self=gl_self.contiguous(),
+        )

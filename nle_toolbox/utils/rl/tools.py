c55ee3b1c9f8c90a4cb0122feb53ed4116cf9218
ivannz <ivannz@yandex.ru>
2022-04-09 00:32:04 +0300

    a proto of an unordered data buffer

--- nle_toolbox/utils/rl/tools.py
+++ nle_toolbox/utils/rl/tools.py
@@ -2,7 +2,9 @@ import plyr
 import torch
 import numpy as np
 
-from collections import defaultdict
+from typing import Any
+
+from collections import defaultdict, deque
 
 
 def cat(arraylike, dim=0):
@@ -82,3 +84,69 @@ class EpisodeExtractor:
         self.strands.clear()
 
         return out
+
+
+class UnorderedLazyBuffer:
+    """A lazily initialized buffer for unordered data.
+    """
+    def __init__(self, capacity: int) -> None:
+        self._buffer = None
+        self.used = deque([], capacity)
+        self.free = set()
+
+    def from_example(self, ex: Any) -> None:
+        def empty(x: Any, *, dim: tuple[int]) -> np.ndarray:
+            # infer the correct basic data type and shape
+            x = np.asanyarray(x)
+            return np.zeros(dim + x.shape, x.dtype)
+
+        # pre-allocate the storage
+        # XXX consider memmapping large buffers
+        #  https://numpy.org/doc/stable/reference/generated/numpy.memmap.html
+        n_slots = self.used.maxlen
+        self._buffer = plyr.apply(empty, ex, dim=(n_slots,))
+
+        # init the free slot stack
+        self.free = set(range(n_slots))
+        self.used.clear()
+
+    def __getitem__(self, index: int) -> Any:
+        """Get the data stored at index.
+        """
+        return plyr.apply(np.take, self._buffer, indices=index, axis=0)
+
+    def __setitem__(self, index: int, ex: Any) -> Any:
+        """Put the data into the storage at the index.
+        """
+        # allow negative indexing
+        j = (index + self.used.maxlen) if index < 0 else index
+
+        # maintain strict data types
+        plyr.apply(lambda x, z: np.copyto(x[j:j+1], z, 'no'), self._buffer, ex)
+
+    def push(self, ex: Any) -> None:
+        """Push the new data into the buffer, optionally evicting the oldest.
+        """
+        # lazily initialize ourselves
+        if self._buffer is None:
+            self.from_example(ex)
+
+        # get the next free slot (genuine or evicted)
+        j = (self.free or self.used).pop()
+        self[j] = ex
+        self.used.appendleft(j)
+
+    def pop(self) -> Any:
+        """Pop the oldest data from the buffer.
+        """
+        if self.used:
+            j = self.used.pop()
+            self.free.add(j)
+            return self[j]
+
+        raise IndexError
+
+    def __bool__(self):
+        """Test whether there is anything in the buffer.
+        """
+        return bool(self.used)
